783fb838ced8a411b37c7b58978c2154986ae4d5
Saheel Das <saheeldas75@gmail.com>
2021-01-24 21:26:49 +0530

    Changed from developement mode to prodcution mode

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path =  require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
@@ -15,5 +16,11 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/v1/transactions',transactions);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client','build','index.html')));
+}
 
 app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
